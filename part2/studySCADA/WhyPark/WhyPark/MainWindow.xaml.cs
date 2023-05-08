792f596ebcf95327d867436823e4aa98b6e3d2cf
Park-JuHyeon <j-hyeon97@naver.com>
2023-05-08 17:18:45 +0900

    0508 팀프로젝트

--- part2/studySCADA/WhyPark/WhyPark/MainWindow.xaml.cs
+++ part2/studySCADA/WhyPark/WhyPark/MainWindow.xaml.cs
@@ -1,22 +1,8 @@
-﻿using Google.Protobuf.WellKnownTypes;
-using MahApps.Metro.Controls;
+﻿using MahApps.Metro.Controls;
 using MySql.Data.MySqlClient;
-using MySqlX.XDevAPI.Relational;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WhyPark.Logics;
 
 namespace WhyPark
@@ -60,8 +46,8 @@ namespace WhyPark
                     cmd.Parameters.AddWithValue("@studentID", TxtStudentId.Text);
                     cmd.Parameters.AddWithValue("@studentName", TxtStudentName.Text);
                     cmd.Parameters.AddWithValue("@reason", TxtReason.Text);
-                    //cmd.Parameters.AddWithValue("@startDate", DPstart.ToString("yyyy-MM-dd"));
-                    //cmd.Parameters.AddWithValue("@endDate", DPend.ToString("yyyy-MM-dd"));
+                    cmd.Parameters.AddWithValue("@startDate", DPstart.Text);
+                    cmd.Parameters.AddWithValue("@endDate", DPend.Text);
                     cmd.ExecuteNonQuery();
                     Debug.WriteLine(query);
                     await Commons.ShowMessageAsync("저장성공", "존나축하");
